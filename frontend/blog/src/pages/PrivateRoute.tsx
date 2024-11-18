15a1f6dcb3d2c6cd72d61d0b809b6cd6b7433ac8
kakimaru <memimei0306@gmail.com>
2024-11-19 07:29:25 +0900

    Add seo title and modify privateroute

--- frontend/blog/src/pages/PrivateRoute.tsx
+++ frontend/blog/src/pages/PrivateRoute.tsx
@@ -1,18 +1,20 @@
-import { Navigate } from 'react-router-dom';
+// import { Navigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
-import { ReactNode, useEffect } from 'react';
+import { ReactNode } from 'react';
+import PageNotFound from './PageNotFound';
 
 const PrivateRoute: React.FC<{ children: ReactNode }> = ({ children }) => {
   const { isLoggedIn } = useAuth();
 
-  useEffect(
-    function () {
-      if (!isLoggedIn) <Navigate to="/" />;
-    },
-    [isLoggedIn],
-  );
+  // useEffect(
+  //   function () {
+  //     if (!isLoggedIn) (<Navigate to="/" />);
+  //   },
+  //   [isLoggedIn],
+  // );
 
   if (isLoggedIn) return children;
+  return <PageNotFound />
 };
 
 export default PrivateRoute;
